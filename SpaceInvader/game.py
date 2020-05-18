51df28f9667a5a550821a5bc18ca72d2c278140c
mhislam1 <mhislam@buffalo.edu>
2020-05-18 00:24:00 -0400

    Add files via upload
    Made the display. Need to add player motion capabilities next.

---
+++ SpaceInvader/game.py
@@ -0,0 +1,34 @@
+import pygame
+
+# Initializes game
+pygame.init()
+
+# Creates 800 x 800 display
+display = pygame.display.set_mode((800,800))
+
+# Title and icon setup
+# Icon credit: <div>Icons made by <a href="https://www.flaticon.com/authors/nhor-phai" title="Nhor Phai">Nhor Phai</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('battleship.png')
+pygame.display.set_icon(icon)
+
+# Fighter
+fighter_image = pygame.image.load('battleship.png')
+fighter_imageX = 400
+fighter_imagey = 650
+
+def player():
+    display.blit(fighter_image, (fighter_imageX, fighter_imagey))
+# Game Runner
+game_running = True
+
+while game_running:
+    # Must be filled before the updates
+    display.fill((0, 0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_running = False
+
+    player()
+    pygame.display.update()
